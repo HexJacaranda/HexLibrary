230a9ec6d585e2596c712f7941147326f51a8047
HexJacaranda <rbqlzg1937n@msn.com>
2018-11-05 16:24:40 +0800

    Rewrite the GC with std::atomic and replaced mutex with spinlock based on atomic_flag

--- GarbageCollector.h
+++ GarbageCollector.h
@@ -1,207 +1,238 @@
 #pragma once
 #include <process.h>
+#include <stdio.h>
+#include <atomic>
 namespace HL
 {
 	//内部保留使用
 	namespace GC
 	{
-		namespace Internal
+		//状态
+		enum class ObjectStatus :unsigned
 		{
-			class GCObjectBase
-			{
-			public:
-				//可到达状态,0为不可到达,1为可到达
-				size_t reachable = 0x1;
-				virtual ~GCObjectBase(){}
-			};
-			template<class T>
-			class GCObject :public GCObjectBase 
-			{
-				T*object_ptr;
-			public:
-				GCObject(T*ptr):object_ptr(ptr){}
-				inline bool IsNull()const {
-					return object_ptr == nullptr;
-				}
-				virtual ~GCObject() {
-					if (object_ptr)
-					{
-						delete object_ptr;
-						object_ptr = nullptr;
-					}
-				}
-			};
-
-			class Tracker
-			{
-			public:
-				GCObjectBase* reference_target;
-				bool is_on_reference;
-				inline static Tracker* NewInstance() {
-					return (Tracker*)System::Memory::Allocator::Alloc(sizeof(Internal::Tracker));
-				}
-			};
-		}
-
-		//垃圾回收器(内部保留使用)
-		class GarbageCollector
+			Reachable = 1,
+			UnReachable = 0
+		};
+		//托管对象基类
+		class ManagedObjectBase
 		{
-			void* scan_thread = nullptr;
-			System::Threading::Volatile lock;
-			size_t state = 0x1;
-			size_t duration = 10;
-			size_t scanindex = 0;
-			System::Threading::Mutex object_enter;
-			System::Generic::Array<Internal::GCObjectBase*>object_table;
-			System::Threading::Mutex handle_enter;
-			System::Generic::Array<Internal::Tracker*>handle_table;
-
-			volatile bool is_changed = false;
-			size_t last_time_count = 0;
-
-			void ScanThread(bool once) 
-			{
-				while (state != 0x0)
-				{
-					Start:
-					if (!once)
-					{
-						if (scanindex++ < duration) 
-						{ 
-							Sleep(1000);
-							continue; 
-						}
-					}
-					if (is_changed == false)//GC是否有更改
-					{
-						if (once)
-						{
-							return;
-						}
-						scanindex = 0;
-						goto Start;
-					}
-					else//有更改的话
-					{
-						state = 0x2;
-						scanindex = 0;
-						this->lock.TryLock();//检查是否有操作在进行
-						//上锁
-						if (handle_table.Count() - last_time_count > handle_table.Count() / 3)//满足一定条件才回收
-						{
-							System::Generic::Array<Internal::GCObjectBase*> sobjs(object_table.Count());
-							System::Generic::Array<Internal::Tracker*> shandles(handle_table.Count());
-
-							for (size_t i = 0; i < handle_table.Count(); ++i)
-							{
-								if (handle_table[i]->is_on_reference)
-								{
-									handle_table[i]->reference_target->reachable = 0x1;
-									shandles.Add(handle_table[i]);
-								}
-								else
-									System::Memory::Allocator::Free(handle_table[i]);
-							}
-							handle_table = static_cast<System::Generic::Array<Internal::Tracker*>&&>(shandles);
-							if (handle_table.Capacity() - handle_table.Count() > handle_table.Count() / 4)//满足条件
-								handle_table.ShrinkToFit();//收缩空间
-							for (size_t i = 0; i < object_table.Count(); ++i)
-							{
-								if (object_table[i]->reachable == 0x1)
-								{
-									object_table[i]->reachable = 0x0;
-									sobjs.Add(object_table[i]);
-								}
-								else
-									delete object_table[i];
-							}
-							object_table = static_cast<System::Generic::Array<Internal::GCObjectBase*>&&>(sobjs);
-							if (object_table.Capacity() - object_table.Count() > object_table.Count() / 4)//满足条件
-								object_table.ShrinkToFit();//收缩空间
-
-							is_changed = false;
-							this->lock.UnLock();//解锁
-							last_time_count = handle_table.Count();//更新count
-							state = 0x01;//更新状态
-							if (once)//如果为一次性扫描
-								return;//退出
-						}				
-					}	
-				}
-			}
-		private:
-			static void ScanProxy(void*arg) {
-				GarbageCollector*ptr = (GarbageCollector*)arg;
-				ptr->ScanThread(false);
-			}
 		public:
-			GarbageCollector() {
-				scan_thread=(void*)_beginthread(ScanProxy, 0, this);
-			}
-			void AddObject(Internal::GCObjectBase*ptr) {
-				if (ptr)
+			ObjectStatus Status = ObjectStatus::UnReachable;
+			virtual ~ManagedObjectBase() { }
+		};
+		//托管对象
+		template<class UDT>
+		class ManagedObject :public ManagedObjectBase
+		{
+		public:
+			UDT*ObjectPtr = nullptr;
+			virtual ~ManagedObject() {
+				if (ObjectPtr != nullptr)
 				{
-					lock.OperationEnter();
-					object_enter.Lock();
-					object_table.Add(ptr);
-					if (!is_changed)
-						is_changed = true;
-					object_enter.UnLock();
-					lock.OperationExit();
-				}
-			}
-			void AddHandle(Internal::Tracker*ptr) {
-				if (ptr)
-				{
-					lock.OperationEnter();
-					handle_enter.Lock();
-					handle_table.Add(ptr);
-					handle_enter.UnLock();
-					lock.OperationExit();
-				}
-			}
-			//立即发起回收动作
-			void CollectNow() {
-				scanindex = 0;
-				ScanThread(true);
-			}
-			~GarbageCollector() {
-				while (state == 0x02)Sleep(0);
-				if (state != 0x00)
-				{
-					state = 0x00;
-					this->lock.OperationEnter();
-					object_enter.Lock();
-					for (size_t i = 0; i < object_table.Count(); ++i)
-						delete object_table[i];
-					object_enter.UnLock();
-					handle_enter.Lock();
-					for (size_t i = 0; i < handle_table.Count(); ++i)
-						System::Memory::Allocator::Free(handle_table[i]);
-					handle_enter.UnLock();
-					this->lock.OperationExit();
-					CloseHandle(this->scan_thread);
+					delete ObjectPtr;
+					ObjectPtr = nullptr;
 				}
 			}
 		};
 
-		//GC全局唯一实体(内部保留使用)
+		enum class HandleType
+		{
+			OnStack,
+			OnHeap
+		};
+		//句柄
+		class ObjectHandle
+		{
+		public:
+			ManagedObjectBase* Target = nullptr;
+			std::atomic<bool> OnReference;
+			std::atomic<bool> OnScanning;
+			HandleType Type = HandleType::OnStack;
+		};
+		//GC状态
+		enum class GCStatus:unsigned
+		{
+			Idle,
+			OnRegularCollecting,
+			OnOnceCollection,
+			Exiting
+		};
+		//回收器
+		class GarbageCollector
+		{
+			void*m_scan_thread = nullptr;
+			System::Threading::Volatile m_access;
+			std::atomic<GCStatus> m_status = GCStatus::Idle;
+			size_t m_duration = 10;
+			size_t m_last = 0;
+			size_t m_duration_index = 0;
+			std::atomic<bool> m_change_flag = false;
+			System::Threading::SpinLock m_object_lock;
+			System::Threading::SpinLock m_handle_lock;
+			System::Generic::Array<ManagedObjectBase*> m_object_table;
+			System::Generic::Array<ObjectHandle*> m_handle_table;
+
+			template<class UDT>
+			inline static UDT* Alloc() {
+				return new UDT;
+			}
+			template<class UDT>
+			inline static void Free(UDT*Target){
+				delete Target;
+			}
+
+			void OnceScan() {
+				if (!(m_status.load(std::memory_order_acquire) == GCStatus::Idle))
+					return;
+				if (!m_change_flag.load(std::memory_order_acquire))
+					return;
+				InternalCollect(GCStatus::OnOnceCollection);
+			}
+			void RegularScan() {
+				for (;;)
+				{
+					if (m_duration_index++ < m_duration)
+					{
+						Sleep(1000);
+						continue;
+					}
+					if (m_change_flag.load(std::memory_order_acquire) == false)
+					{
+						m_duration_index = 0;
+						continue;
+					}
+					else
+					{
+						InternalCollect(GCStatus::OnRegularCollecting);
+						m_duration_index = 0;
+					}
+				}
+			}
+			void InternalCollect(GCStatus Status) {
+				m_access.TryLock();
+				m_status.store(Status, std::memory_order_release);
+				if (!(m_handle_table.Count() - m_last > m_handle_table.Count() / 2))//检查数量
+					goto Exit;
+				{
+					System::Generic::Array<ManagedObjectBase*> survived_object(m_object_table.Count());
+					System::Generic::Array<ObjectHandle*> survived_handle(m_handle_table.Count());
+					for (index_t i = 0; i < m_handle_table.Count(); ++i) {
+						if (m_handle_table[i]->Type == HandleType::OnStack) {
+							if (m_handle_table[i]->OnReference.load(std::memory_order_acquire) == true)
+							{
+								m_handle_table[i]->Target->Status = ObjectStatus::Reachable;
+								survived_handle.Add(m_handle_table[i]);
+							}
+							else
+								Free(m_handle_table[i]);
+						}
+					}
+					for (index_t i = 0; i < m_object_table.Count(); ++i) {
+						if (m_object_table[i]->Status == ObjectStatus::Reachable) {
+							m_object_table[i]->Status = ObjectStatus::UnReachable;
+							survived_object.Add(m_object_table[i]);
+						}
+						else
+							Free(m_object_table[i]);
+					}
+					m_object_table = System::Forward(survived_object);
+					m_handle_table = System::Forward(survived_handle);
+					if (m_handle_table.Capacity() - m_handle_table.Count() > m_handle_table.Count() / 4)//满足条件
+						m_handle_table.ShrinkToFit();//收缩空间
+					if (m_object_table.Capacity() - m_object_table.Count() > m_object_table.Count() / 4)//满足条件
+						m_object_table.ShrinkToFit();//收缩空间
+					m_last = m_handle_table.Count();
+				}
+			Exit:
+				m_status.store(GCStatus::Idle, std::memory_order_release);
+				m_change_flag.store(false, std::memory_order_release);
+				m_access.UnLock();
+			}
+			static void ScanProxy(void*args) {
+				GarbageCollector*ptr = (GarbageCollector*)args;
+				ptr->RegularScan();
+			}
+		public:
+			GarbageCollector() {
+				m_scan_thread = (void*)_beginthread(ScanProxy, 0, this);
+			}
+			//获取Handle
+			ObjectHandle* GetObjectHandle(ManagedObjectBase*Target, HandleType Type = HandleType::OnStack) {
+				ObjectHandle*ret = Alloc<ObjectHandle>();
+				ret->OnReference.store(true, std::memory_order_release);
+				ret->Target = Target;
+				return ret;
+			}
+			//添加Handle
+			void AddHandle(ObjectHandle*Target) {
+				m_access.OperationEnter();
+				m_handle_lock.Lock();
+				m_change_flag.store(true, std::memory_order_release);
+				m_handle_table.Add(Target);
+				m_handle_lock.Unlock();
+				m_access.OperationExit();
+			}
+			//获取托管对象
+			template<class UDT>
+			ManagedObject<UDT>* GetManagedObject(UDT*Target) {
+				ManagedObject<UDT>*ret = Alloc<ManagedObject<UDT>>();
+				ret->ObjectPtr = Target;
+				return ret;
+			}
+			//添加Object
+			void AddObject(ManagedObjectBase*Target) {
+				m_access.OperationEnter();
+				m_object_lock.Lock();
+				m_change_flag.store(true, std::memory_order_release);
+				m_object_table.Add(Target);
+				m_object_lock.Unlock();
+				m_access.OperationExit();
+			}
+			//从指定句柄添加引用
+			ObjectHandle* AddReferenceFrom(ObjectHandle*Target) {
+				ObjectHandle*ret = Alloc<ObjectHandle>();
+				ret->OnReference.store(true, std::memory_order_release);
+				m_access.OperationEnter();
+				m_object_lock.Lock();
+				m_change_flag.store(true, std::memory_order_release);
+				ret->Target = Target->Target;
+				m_object_lock.Unlock();
+				m_access.OperationExit();
+				AddHandle(ret);
+				return ret;
+			}
+			//析构
+			~GarbageCollector() {
+				while (m_status.load(std::memory_order_acquire) != GCStatus::Idle)Sleep(0);
+				this->m_access.OperationEnter();
+				m_object_lock.Lock();
+				for (size_t i = 0; i < m_object_table.Count(); ++i)
+					Free(m_object_table[i]);
+				m_object_lock.Unlock();
+				m_handle_lock.Lock();
+				for (size_t i = 0; i < m_handle_table.Count(); ++i)
+					Free(m_handle_table[i]);
+				m_handle_lock.Unlock();
+				this->m_access.OperationExit();
+				CloseHandle(m_scan_thread);
+			}
+		};
+
 		static GarbageCollector& GC() {
 			static GarbageCollector gc;
 			return gc;
 		}
-
 		//uptr对接GC配置
 		namespace InternalAdopt
 		{
 			class managed_resource_base :public System::UPointer::uptr_resource
 			{
 			public:
-				Internal::Tracker* tracker = nullptr;
+				ObjectHandle* tracker = nullptr;
 				virtual void release() final{
 					if (tracker)
 					{
-						tracker->is_on_reference = false;
+						tracker->OnReference.store(false, std::memory_order_release);
 						tracker = nullptr;
 					}
 				}
@@ -217,11 +248,8 @@ namespace HL
 					InternalAdopt::managed_resource<T> * ptr = new InternalAdopt::managed_resource<T>;
 					T*object_ptr = (T*)this->object;
 					T*new_object = System::Interface::ICloneable::GetClonePtr(*object_ptr);
-					Internal::GCObjectBase*base = new Internal::GCObject<T>(new_object);
-					ptr->tracker = Internal::Tracker::NewInstance();
-					ptr->tracker->reference_target = base;
-					ptr->tracker->is_on_reference = true;
-					ptr->object = new_object;
+					ManagedObjectBase*base = GC().GetManagedObject<T>(new_object);
+					ptr->tracker = GC().GetObjectHandle(base);
 					GC().AddHandle(ptr->tracker);
 					GC().AddObject(base);
 					return ptr;
@@ -251,10 +279,7 @@ namespace HL
 						managed_resource_base*objptr = static_cast<managed_resource_base*>(ptr);
 						managed_resource_base*lptr = objptr->selfclone();
 						lptr->object = objptr->object;
-						lptr->tracker = Internal::Tracker::NewInstance();
-						lptr->tracker->is_on_reference = true;
-						lptr->tracker->reference_target = objptr->tracker->reference_target;
-						GC().AddHandle(lptr->tracker);
+						lptr->tracker = GC().AddReferenceFrom(objptr->tracker);
 						return lptr;
 					}
 					else
@@ -268,9 +293,7 @@ namespace HL
 					}
 					else
 						return true;
-					
 				}
-
 			};
 		}
 
@@ -279,11 +302,9 @@ namespace HL
 			InternalAdopt::managed_resource<U> * ptr = new InternalAdopt::managed_resource<U>;
 			InternalAdopt::managed_keeper *lptr = System::UPointer::keeper_interface<InternalAdopt::managed_keeper>();
 			U* object_ptr = new U(const_cast<Args&>(args)...);
-			Internal::GCObjectBase*base = new Internal::GCObject<U>(object_ptr);
-			ptr->tracker = Internal::Tracker::NewInstance();
-			ptr->tracker->reference_target = base;
-			ptr->tracker->is_on_reference = true;
 			ptr->object = object_ptr;
+			ManagedObjectBase* base = GC().GetManagedObject<U>(object_ptr);
+			ptr->tracker = GC().GetObjectHandle(base);
 			GC().AddHandle(ptr->tracker);
 			GC().AddObject(base);
 			return HL::System::UPointer::uptr<U>(object_ptr, ptr, lptr);
